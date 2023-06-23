8071dc222c2febbc99e363df7a2468a757e64968
Amanda Folson <amanda@nstream.io>
2023-06-22 23:16:44 -0400

    🤷‍♀️

--- _plugins/linkchecker.rb
+++ _plugins/linkchecker.rb
@@ -1,13 +1,13 @@
-require "html-proofer"
+# require "html-proofer"
 
-Jekyll::Hooks.register :site, :post_write do |site|
-  HTMLProofer.check_directory(site.config["destination"], opts = {
-    :allow_hash_href => false,
-    :check_html => true,
-    :disable_external => true,
-    :enforce_https => false,
-    :ignore_empty_alt => false,
-    :ignore_missing_alt => false,
-    :url_swap => "^/" + site.config["baseurl"] + "/:/"
-  }).run
-end
+# Jekyll::Hooks.register :site, :post_write do |site|
+#   HTMLProofer.check_directory(site.config["destination"], opts = {
+#     :allow_hash_href => false,
+#     :check_html => true,
+#     :disable_external => true,
+#     :enforce_https => false,
+#     :ignore_empty_alt => false,
+#     :ignore_missing_alt => false,
+#     :url_swap => "^/" + site.config["baseurl"] + "/:/"
+#   }).run
+# end
