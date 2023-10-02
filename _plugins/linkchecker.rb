70800d5de86e4abaa726f2fe6e4d23467130f032
Amanda Folson <afolson@users.noreply.github.com>
2023-10-02 13:22:23 -0400

    Update linkchecker.rb (#112)

--- _plugins/linkchecker.rb
+++ _plugins/linkchecker.rb
@@ -1,13 +1,16 @@
-# require "html-proofer"
+require "html-proofer"
 
-# Jekyll::Hooks.register :site, :post_write do |site|
-#   HTMLProofer.check_directory(site.config["destination"], opts = {
-#     :allow_hash_href => false,
-#     :check_html => true,
-#     :disable_external => true,
-#     :enforce_https => false,
-#     :ignore_empty_alt => false,
-#     :ignore_missing_alt => false,
-#     :url_swap => "^/" + site.config["baseurl"] + "/:/"
-#   }).run
-# end
+Jekyll::Hooks.register :site, :post_write do |site|
+  HTMLProofer.check_directory(site.config["destination"], opts = {
+    :allow_hash_href => false,
+    :allow_missing_href => true,
+    :check_html => true,
+    :disable_external => true,
+    :enforce_https => false,
+    :ignore_empty_alt => true,
+    :ignore_missing_alt => true,
+    :swap_urls => {
+      Regexp.new("^" + site.config["baseurl"]) => ""
+    }
+  }).run
+end
